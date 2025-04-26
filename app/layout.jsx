c792835a1706f073edd7079317041e3cdce3fe31
radu-bordea <radu.bordea.dev@gmail.com>
2025-04-26 19:07:51 +0200

    add navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,17 @@
 import "@/assets/styles/globals.css";
+import NavBar from "./components/Navbar";
 
 export const metadata = {
-    title: 'Property Pulse',
-    keywords: 'rental, property, real estate',
-    description: 'Find the perfect rental property'
-}
+  title: "Property Pulse",
+  keywords: "rental, property, real estate",
+  description: "Find the perfect rental property",
+};
 
 const MainLayout = ({ children }) => {
   return (
     <html>
       <body>
+        <NavBar />
         <main>{children}</main>
       </body>
     </html>
