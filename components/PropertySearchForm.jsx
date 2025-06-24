7e9a37a4060acac6a2a8e1c761b5277cc24372a7
radu-bordea <radu.bordea.dev@gmail.com>
2025-06-24 10:06:00 +0200

    fetch and display property search

--- components/PropertySearchForm.jsx
+++ components/PropertySearchForm.jsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
@@ -10,18 +9,22 @@ const PropertySearchForm = () => {
   const router = useRouter();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if(location === '' && propertyType === "All") {
-        router.push("/properties")
+    if (location === "" && propertyType === "All") {
+      router.push("/properties");
     } else {
-        const query = `?location=${location}&propertyType=${propertyType}`
-        router.push(`/properties/search-results${query}`)
+      const query = `?location=${location}&propertyType=${propertyType}`;
+
+      router.push(`/properties/search-results${query}`);
     }
-  }
+  };
 
   return (
-    <form onSubmit={handleSubmit} className="mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center">
+    <form
+      onSubmit={handleSubmit}
+      className="mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center"
+    >
       <div className="w-full md:w-3/5 md:pr-2 mb-4 md:mb-0">
         <label htmlFor="location" className="sr-only">
           Location
@@ -29,13 +32,12 @@ const PropertySearchForm = () => {
         <input
           type="text"
           id="location"
-          placeholder="Enter Location (City, State, Zip, etc)"
+          placeholder="Enter Keywords or Location"
           className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-blue-500"
           value={location}
           onChange={(e) => setLocation(e.target.value)}
         />
       </div>
-
       <div className="w-full md:w-2/5 md:pl-2">
         <label htmlFor="property-type" className="sr-only">
           Property Type
@@ -57,7 +59,6 @@ const PropertySearchForm = () => {
           <option value="Other">Other</option>
         </select>
       </div>
-
       <button
         type="submit"
         className="md:ml-4 mt-4 md:mt-0 w-full md:w-auto px-6 py-3 rounded-lg bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-500"
